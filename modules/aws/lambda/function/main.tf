d4a085a77f09ae732d55c2224fffb44bac613269
sravanigaju <sravanigajula99@gmail.com>
2026-03-30 10:26:01 +0530

    add sagemaker endpoint module

--- modules/aws/lambda/function/main.tf
+++ modules/aws/lambda/function/main.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "this" {
-  function_name = var.name
+  function_name = var.function_name
   role          = var.role_arn
   handler       = var.handler
   runtime       = var.runtime
--- modules/aws/lambda/function/variables.tf
+++ modules/aws/lambda/function/variables.tf
@@ -1,5 +1,24 @@
-variable "name" {}
-variable "role_arn" {}
-variable "handler" {}
-variable "runtime" {}
-variable "filename" {}
+variable "function_name" {
+  type        = string
+  description = "Name of the Lambda function"
+}
+
+variable "role_arn" {
+  type        = string
+  description = "IAM role ARN for Lambda execution"
+}
+
+variable "handler" {
+  type        = string
+  description = "Lambda handler (e.g., index.handler)"
+}
+
+variable "runtime" {
+  type        = string
+  description = "Lambda runtime (e.g., nodejs18.x, python3.10)"
+}
+
+variable "filename" {
+  type        = string
+  description = "Path to the deployment package zip file"
+}
